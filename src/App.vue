d888fb9eb5e5fc160a1b57e369a532e04381336e
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-08-26 17:28:09 +0600

    add function for responsive, delete nav-router-links, some styling

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@function vw($value, $base: 1920) {
+  @return $value / $base * 100vw;
 }
 
-#nav {
-  padding: 30px;
+@function vh($value, $base: 800) {
+  @return $value / $base * 100vh;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+@function vmin($value, $base: 375) {
+  @return $value / $base * 100vmin;
+}
+body {
+  font-family: "Roboto", "Open Sans", system-ui, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c2c2c;
+  background-color: #f6f7f8;
+}
+.container {
+  width: vw(900);
+  margin: vw(50) auto;
 }
 </style>
